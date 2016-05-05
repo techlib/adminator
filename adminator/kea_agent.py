d9b56cca8a1fe57850c0061669f6d66a8fe7d1b4
Jan Dvořák <mordae@mordae.eu>
2016-05-05 11:17:23 +0200

    kea-agent-daemon: implement config deployment
    Signed-off-by: Jan Dvořák <mordae@mordae.eu>

--- adminator/kea_agent.py
+++ adminator/kea_agent.py
@@ -3,10 +3,9 @@
 
 __all__ = ['KeaAgent']
 
+from os import system
 from twisted.python import log
-
 from simplejson import dump, load
-
 from adminator.kea import generate_kea_config, DEFAULTS
 
 
@@ -15,6 +14,7 @@ class KeaAgent(object):
         self.db = db
 
         if template is not None:
+            log.msg('Reading Template {}'.format(template))
             with open(template) as fp:
                 self.template = load(fp)
         else:
@@ -23,9 +23,25 @@ class KeaAgent(object):
         self.output = output or '/etc/kea/kea.conf'
         self.signal = signal or 'keactrl reload'
 
+    def start(self):
+        self.update()
 
     def notify(self, event):
         log.msg('Database Notification: {}'.format(event.channel))
+
+        if event.channel == 'dhcp':
+            self.update()
+
+    def update(self):
+        log.msg('Generating Configuration')
+        config = generate_kea_config(self.db, self.template)
+
+        log.msg('Writing: {}'.format(self.output))
+        with open(self.output, 'w') as fp:
+            dump(config, fp)
+
+        log.msg('Executing: {}'.format(self.signal))
+        system(self.signal)
 
 
 # vim:set sw=4 ts=4 et:
