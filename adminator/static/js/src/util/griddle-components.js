32119797e11f7e547cd2d1be0b4bf8192ce6af94
Jakub Koudelka <koudelka.j@gmail.com>
2016-05-23 17:02:25 +0200

    Fixed griddle search function (do not duplicate results)
    Signed-off-by: Jakub Koudelka <koudelka.j@gmail.com>

--- adminator/static/js/src/util/griddle-components.js
+++ adminator/static/js/src/util/griddle-components.js
@@ -1,32 +1,35 @@
 function regexGridFilter(rows, filter) {
-  var filterArr = s.trim(filter).split(' ');
-  var results = []
+	var filterArr = s.trim(filter).split(' ');
 
-  _.each(filterArr, (match) => {
-    _.each(rows, (row,row_id) => {
-      _.each(row, (v,k) => {
-        if(_.isArray(v)){
-                v = _.map(v, _.values).toString()
-        }
-        if(_.isObject(v)){
-                v = _.values(v).toString()
-        }
-        if(match.substr(0,1) == '/'){
-          var re = new RegExp(match.substr(1, match.length-1))
-          if( ( v || "").toString().search(re) >= 0 ){
-            results.push(row)
-            return
-          }
-        } else {
-          if( (v || "").toString().toLowerCase().indexOf(match.toLowerCase()) >= 0 ){
-            results.push(row)
-            return
-          }
-        }
-      })
-    })
-    rows = results
-    results = []
-  })
-  return rows
+	return _.filter(rows, (row, row_id) => {
+        var found = false
+        _.forEach(filterArr, (match) => {
+            _.forEach(row, (v,k) => {
+
+				if (_.isArray(v)) {
+                    v = _.map(v, _.values).toString()
+				}
+				if (_.isObject(v)) {
+					v = _.values(v).toString()
+				}
+
+				if (match.substr(0,1) == '/') {
+					var re = new RegExp(match.substr(1, match.length-1))
+                    if( ( v || "").toString().search(re) >= 0 ){
+                        found = true
+                    }
+                } else {
+					if( (v || "").toString()
+						.toLowerCase().indexOf(match.toLowerCase()) >= 0 )
+					{
+                        found = true
+                    }
+                }
+            })
+            if (found) {
+				return false;
+			}
+		})
+        return found;
+	})
 }
