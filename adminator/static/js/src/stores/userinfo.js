76cc0887a33eb446c0d2cbfe848b9a60809c36b8
Jakub Koudelka <koudelka.j@gmail.com>
2016-06-30 17:16:38 +0200

    Added basic privilige checking functions to userinfo store
    Signed-off-by: Jakub Koudelka <koudelka.j@gmail.com>

--- adminator/static/js/src/stores/userinfo.js
+++ adminator/static/js/src/stores/userinfo.js
@@ -20,4 +20,24 @@ var UserInfoStore = Reflux.createStore({
             }
         })
     },
+
+	isAllowed(privilegesAllowed) {
+        if (!_.isArray(privilegesAllowed)) {
+            privilegesAllowed = _.map(privilegesAllowed.split(','), (i) => i.trim())
+        }
+        privilegesAllowed.push('admin') // admin is always allowed
+        return _.intersection(this.data.privileges, privilegesAllowed).length > 0
+	},
+
+	getDeviceTypePermissions() {
+        return null;
+        /*
+		return {
+            'visitor': ['0b645942-c6b9-4e21-9c97-c47f2524e9ea'],
+			'device':  ['5e269bd2-be39-49f9-9810-519529fdc86c',
+                        'a4e8d238-9f6d-44ac-8444-176a568a0cbd'],
+		}
+       */
+	},
+
 })
--- adminator/static/js/stores/userinfo.js
+++ adminator/static/js/stores/userinfo.js
@@ -21,5 +21,27 @@ var UserInfoStore = Reflux.createStore({
                 FeedbackActions.set('error', result.responseJSON.message);
             }
         });
+    },
+
+    isAllowed: function isAllowed(privilegesAllowed) {
+        if (!_.isArray(privilegesAllowed)) {
+            privilegesAllowed = _.map(privilegesAllowed.split(','), function (i) {
+                return i.trim();
+            });
+        }
+        privilegesAllowed.push('admin'); // admin is always allowed
+        return _.intersection(this.data.privileges, privilegesAllowed).length > 0;
+    },
+
+    getDeviceTypePermissions: function getDeviceTypePermissions() {
+        return null;
+        /*
+        return {
+            'visitor': ['0b645942-c6b9-4e21-9c97-c47f2524e9ea'],
+        'device':  ['5e269bd2-be39-49f9-9810-519529fdc86c',
+                        'a4e8d238-9f6d-44ac-8444-176a568a0cbd'],
+        }
+        */
     }
+
 });
