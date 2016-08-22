1f28976417c9b1e406eff40df63db4b482cca5b8
Ondřej Koch <o.koch@zerusnet.com>
2016-08-22 11:25:49 +0200

    First and last pagination options

--- adminator/static/js/src/components/Pager.jsx
+++ adminator/static/js/src/components/Pager.jsx
@@ -29,6 +29,13 @@ export var Pager = React.createClass({
         startIndex = endIndex - 11
       }
 
+      var firstActive = startIndex > 0 ? '': 'disabled'
+      var first = <li className={firstActive}><a onClick={this.pageChange} data-value='0'><i className="glyphicon glyphicon-step-backward"></i>First</a></li>
+
+      var lastActive = endIndex < this.props.maxPage ? '' : 'disabled'
+      var last = <li className={lastActive}><a onClick={this.pageChange} data-value={this.props.maxPage - 1}>Last <i className="glyphicon glyphicon-step-forward"></i></a></li>
+
+
         for(var i = startIndex; i < endIndex;  i++) {
           var selected = this.props.currentPage == i ? 'active' : ''
             options.push(<li key={i} className={selected}><a onClick={this.pageChange} data-value={i}>{i + 1}</a></li>)
@@ -36,9 +43,9 @@ export var Pager = React.createClass({
 
         return (
           <div className="row">
-            <div className="col-md-12">
+            <div className="col-md-11">
               <ul className="pagination noselect">
-                {previous}{options}{next}
+                {first}{previous}{options}{next}{last}
               </ul>
             </div>
           </div>
