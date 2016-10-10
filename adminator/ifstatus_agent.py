23a74c7eb1cea3079928347073a148c6b42270d8
Miroslav Brabenec <brabemi@gmail.com>
2016-10-10 16:07:18 +0200

    Parallel update
    update has been rewritten to parallel version. Thread is created for each switch to get data over snmp. Data are agregated and in single core commited to database.

--- adminator/ifstatus_agent.py
+++ adminator/ifstatus_agent.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3 -tt
 # -*- coding: utf-8 -*-
 
-# from twisted.internet import reactor
 from twisted.internet import task
 from twisted.python import log
 
 import subprocess
 import datetime
 from sqlalchemy import and_
+import threading
+import time
 
 __all__ = ['IFStatusAgent']
 
@@ -15,6 +16,7 @@ __all__ = ['IFStatusAgent']
 class IFStatusAgent(object):
 	def __init__(self, db):
 		self.db = db
+		self._lock = threading.Lock()
 
 	def start(self):
 		"""Start the periodic checking."""
@@ -31,7 +33,6 @@ class IFStatusAgent(object):
 			'LastChange':('1.3.6.1.2.1.2.2.1.9', '#IF-MIB::ifLastChange.4227913 = Timeticks: (1284900111) 148 days, 17:10:01.11', '', 1),
 			'Name':('1.3.6.1.2.1.31.1.1.1.1', '#IF-MIB::ifName.4227913 = STRING: GigabitEthernet1/0/37', 'STRING: ', 1),
 			'mapping-1':('1.3.6.1.2.1.17.1.4.1.2', '#mapping - SNMPv2-SMI::mib-2.17.1.4.1.2.37 = INTEGER: 4227913', 'INTEGER: ', 2),
-			#~ 'MAC':('1.3.6.1.2.1.17.4.3.1.1', '#SNMPv2-SMI::mib-2.17.4.3.1.1.24.169.5.52.121.109 = Hex-STRING: 18 A9 05 34 79 6D ', 'Hex-STRING: ', 3),
 			'Vlan':('1.3.6.1.2.1.17.7.1.4.5.1.1', '#SNMPv2-SMI::mib-2.17.7.1.4.5.1.1.4227913 = Gauge32: 13', 'Gauge32: ', 1),
 			'mapping-2':('1.3.6.1.2.1.17.4.3.1.2', '#mapping - SNMPv2-SMI::mib-2.17.7.1.2.2.1.2.13.24.169.5.52.121.109 = INTEGER: 37', 'INTEGER: ', 3),
 		}
@@ -82,48 +83,56 @@ class IFStatusAgent(object):
 
 		return  mapped_vals[1]
 
+	def save_to_db(self, switch, data):
+		for key, val in data.items():
+			if not 'Vlan' in val:
+				val['Vlan'] = 0
+			where = and_(self.db.interface.switch == switch.uuid, self.db.interface.name == val['Name'])
+			#~ TODO:
+			#~ no row (new sw in stack, etc) or multiple ((switch, name) is uniq pair -> no multiple)
+			#~ http://docs.sqlalchemy.org/en/latest/orm/query.html#sqlalchemy.orm.query.Query.one
+			interface = self.db.interface.filter(where).one()
+
+			interface.admin_status = val['AdminStatus']
+			interface.oper_status = val['OperStatus']
+			interface.speed = int(val['Speed'])
+			interface.vlan = int(val['Vlan'])
+			self.db.mac_address.filter_by(interface = interface.uuid).delete()
+			if interface.ignore_macs is False:
+				for mac in val['MACs']:
+					self.db.mac_address.insert(
+						mac_address = mac,
+						interface = interface.uuid,
+					)
+		switch.last_update = datetime.datetime.now()
+		self.db.commit()
+
+	def parallel_update(self, switch, snmp_profile, output):
+		start = time.time()
+		output[switch.uuid] = self.get_interfaces(
+			switch.ip_address, snmp_profile.version,
+			snmp_profile.community, snmp_profile.timeout
+		)
+		log.msg('{} finished ({:.03f} s)'.format(switch.name, time.time() - start))
+
 	def update(self):
 		log.msg('Interface status sync started')
+		start = time.time()
+		threads = []
 		data = {}
 		for switch in self.db.switch.filter_by(enable = True).all():
+			data[switch.uuid] = None
 			snmp_profile = self.db.snmp_profile.get(switch.snmp_profile)
-			#~ reactor.callInThread(
-				#~ data[switch.uuid] = self.get_interfaces(
-					#~ switch.ip_address, snmp_profile.version,
-					#~ snmp_profile.community, snmp_profile.timeout
-				#~ ) 
-			#~ )
-			data[switch.uuid] = self.get_interfaces(
-				switch.ip_address, snmp_profile.version,
-				snmp_profile.community, snmp_profile.timeout
-			)
+			t = threading.Thread(target=self.parallel_update, args=(switch,snmp_profile,data))
+			t.start()
+			threads.append(t)
 
-		
+		for t in threads:
+			t.join()
 
 		for switch in self.db.switch.filter_by(enable = True).all():
-			for key, val in data[switch.uuid].items():
-				if not 'Vlan' in val:
-					val['Vlan'] = 0
-				where = and_(self.db.interface.switch == switch.uuid, self.db.interface.name == val['Name'])
-				#~ TODO:
-				#~ no row (new sw in stack, etc) or multiple ((switch, name) is uniq pair -> no multiple)
-				#~ http://docs.sqlalchemy.org/en/latest/orm/query.html#sqlalchemy.orm.query.Query.one
-				interface = self.db.interface.filter(where).one()
-
-				interface.admin_status = val['AdminStatus']
-				interface.oper_status = val['OperStatus']
-				interface.speed = int(val['Speed'])
-				interface.vlan = int(val['Vlan'])
-				self.db.mac_address.filter_by(interface = interface.uuid).delete()
-				if interface.ignore_macs is False:
-					for mac in val['MACs']:
-						self.db.mac_address.insert(
-							mac_address = mac,
-							interface = interface.uuid,
-						)
-			switch.last_update = datetime.datetime.now()
-			self.db.commit()
-		log.msg('Interface status sync finished')
+			self.save_to_db(switch, data[switch.uuid])
+		log.msg('Interface status sync finished ({:.03f} s)'.format(time.time() - start))
 
 
 # vim:set sw=4 ts=4 et:
