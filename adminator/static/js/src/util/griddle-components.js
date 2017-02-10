91b7167660ce92d081ca9880d82e62536acaf4c8
Miroslav Brabenec <brabemi@gmail.com>
2017-02-10 10:49:51 +0100

    Filter fix
    lodash _.values(object): _.values('hi'); => ['h', 'i']
    fixed coding style, why do you combine tabs and spaces

--- adminator/static/js/src/util/griddle-components.js
+++ adminator/static/js/src/util/griddle-components.js
@@ -4,35 +4,30 @@ import {trim} from 'underscore.string'
 export function regexGridFilter(rows, filter) {
 	var filterArr = trim(filter).split(' ')
 
-	return _.filter(rows, (row) => {
-        var found = true
-        _.forEach(filterArr, (match) => {
-			var filterWordFound = false
-            _.forEach(row, (v) => {
+  return _.filter(rows, (row) => {
+    var found = true
+    _.forEach(filterArr, (match) => {
+      var filterWordFound = false
+       _.forEach(row, (v) => {
+        if (_.isArray(v)) {
+          v = _.map(v, function(e) {return _.isString(e) ? e :  _.values(e)}).toString()
+        }
+        if (_.isObject(v)) {
+          v = _.values(v).toString()
+         }
 
-				if (_.isArray(v)) {
-                    v = _.map(v, _.values).toString()
-				}
-				if (_.isObject(v)) {
-					v = _.values(v).toString()
-				}
-
-				if (match.substr(0,1) == '/') {
-					var re = new RegExp(match.substr(1, match.length-1))
-                    if( ( v || '').toString().search(re) >= 0 ) {
-                        filterWordFound = true
-                    }
-                } else {
-					if( (v || '').toString()
-						.toLowerCase().indexOf(match.toLowerCase()) >= 0 )
-					{
-                        filterWordFound = true
-                    }
-                }
-				if (filterWordFound) {return true}
-            })
-			found = found && filterWordFound
-		})
-        return found
-	})
+        if (match.substr(0,1) == '/') {
+          var re = new RegExp(match.substr(1, match.length-1))
+          if( ( v || '').toString().search(re) >= 0 ) { filterWordFound = true }
+        } else {
+          if( (v || '').toString().toLowerCase().indexOf(match.toLowerCase()) >= 0 ) {
+            filterWordFound = true
+          }
+        }
+        if (filterWordFound) {return true}
+      })
+      found = found && filterWordFound
+    })
+    return found
+  })
 }
